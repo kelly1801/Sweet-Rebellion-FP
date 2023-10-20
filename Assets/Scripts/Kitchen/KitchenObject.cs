47ae644a1bcb3eedc1eabbc671db2d8d54cc7e07
Kelly Chara <fabianachara@gmail.com>
2023-10-19 22:17:31 -0500

    Add scriptable objects for kitchen elements

---
+++ Assets/Scripts/Kitchen/KitchenObject.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class KitchenObject : MonoBehaviour
+{
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    private IKitchenElementParent kitchenElementParent;
+
+    public KitchenObjectSO GetKitchenObject()
+    {
+        return kitchenObjectSO;
+    }
+
+    public void SetKitchenObjectParent(IKitchenElementParent kitchenElementParent)
+    {
+        if (this.kitchenElementParent != null)
+        {
+            this.kitchenElementParent.ClearKitchenObject();
+
+
+            this.kitchenElementParent = kitchenElementParent;
+
+            kitchenElementParent.SetKitchenObject(this);
+
+            transform.parent = kitchenElementParent.GetKitchenElementNewTransform();
+            transform.localPosition = Vector3.zero;
+        }
+
+    }
+
+    public IKitchenElementParent GetKitchenElementParent()
+    {
+        return kitchenElementParent;
+    }
+}
